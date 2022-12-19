0eb62ae5e5b0e77ccfa2e8cc16aea1c25619dcbe
mjaenm <120589287+mjaenm@users.noreply.github.com>
2022-12-19 18:06:39 +0100

    Add files via upload

---
+++ eureka-service/src/test/java/com/nttdata/bootcamp/eurekaservice19/EurekaService19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.eurekaservice19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaService19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
